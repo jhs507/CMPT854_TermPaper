c337db1b89ed106d4460de4ad0168015d809264b
jhs507 <jhs507@mail.usask.ca>
2022-11-03 13:56:11 -0600

    Added comments to ContributorDatabase class

--- contributor_database.py
+++ contributor_database.py
@@ -2,7 +2,14 @@ from contributor import Contributor
 from functools import cmp_to_key
 
 class ContributorDatabase(object):
-    """Holds contributor objects and provides utilites to manipulate them."""
+    """Holds contributor objects and provides utilites to manipulate them.
+
+    Fields:
+    list_of_contributors - list of contributors sorted by number of commits
+
+    Methods:
+    add_contributor(contributor) - Adds contributor to the list of contributors.
+    """
 
     def __init__(self):
         super(ContributorDatabase, self).__init__()
@@ -10,6 +17,10 @@ class ContributorDatabase(object):
 
 
     def add_contributor(self, contributor):
+        """Adds contributor to list of contributors.
+
+        Sorts the list of contributors decending by number of commits.
+        """
         self.list_of_contributors.append(contributor)
         self.list_of_contributors.sort(
             key=cmp_to_key(Contributor.compare),
