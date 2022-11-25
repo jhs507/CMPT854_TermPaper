c6e1ea6154fce4805814f9e54bd09dd844f43741
jhs507 <jhs507@mail.usask.ca>
2022-11-25 16:14:50 -0600

    Added list of projects to analyze.

--- main.py
+++ main.py
@@ -117,7 +117,51 @@ def main():
     print("Project Name,Contributor Name,Contributor Code,Initial Commit Hash,Commits in first six months,lines of code in first six months,Initial lines of code,Inital Comment Lines of Code,Initial average CCN,Number of Functions,Number of functions with CCN > 10,Comment Ratio,Modularity Level,Documentation Level,Presence of Install Guide,Presence of Build Guide,Presence of Getting Started Guide")
 
     tuples = [
-        ("mines/abseil-cpp", "master", "abseil/abseil-cpp")
+        ("mines/rock-kernel-driver","master","radeonopencompute/rock-kernel-driver"),
+        ("mines/alios-things","master","alibaba/alios-things"),
+        #("mines/vk-gl-cts","main","khronosgroup/vk-gl-cts"),
+        ("mines/caseflow","master","department-of-veterans-affairs/caseflow"),
+        ("mines/snorkel","main","snorkel-team/snorkel"),
+        ("mines/devito","master","devitocodes/devito"),
+        ("mines/sqltoolsservice","main","microsoft/sqltoolsservice"),
+        ("mines/openhab-docs","main","openhab/openhab-docs")
+        ("mines/botbuilder-samples","main","microsoft/botbuilder-samples"),
+        ("mines/decrediton","master","decred/decrediton"),
+        ("mines/oneflow","master","oneflow-inc/oneflow"),
+        ("mines/picocli","main","remkop/picocli"),
+        ("mines/service-ui","master","reportportal/service-ui"),
+        ("mines/dde-dock","master","linuxdeepin/dde-dock"),
+        ("mines/mythril","develop","consensys/mythril"),    ]
+        ("mines/kimai2","master","kevinpapst/kimai2"),
+        ("mines/ovn-kubernetes","master","ovn-org/ovn-kubernetes"),
+        ("mines/tasks","master","nextcloud/tasks"),
+        ("mines/symplify","main","symplify/symplify"),
+        ("mines/atrium","main","robstoll/atrium"),
+        ("mines/redex","main","facebook/redex"),
+        ("mines/swift-snapshot-testing","main","pointfreeco/swift-snapshot-testing"),
+        ("mines/terraform-provider-cloudflare","master","cloudflare/terraform-provider-cloudflare"),
+        ("mines/swift-protobuf","main","apple/swift-protobuf"),
+        ("mines/continuous-integration","master","bazelbuild/continuous-integration"),
+        ("mines/publishpress","development","publishpress/publishpress"),
+        ("mines/appdaemon","dev","appdaemon/appdaemon"),
+        ("mines/falco","master","falcosecurity/falco"),
+        ("mines/icingaweb2-module-director","master","icinga/icingaweb2-module-director"),
+        ("mines/mjml","master","mjmlio/mjml"),
+        ("mines/redisjson","master","redisjson/redisjson"),
+        ("mines/sled","main","spacejam/sled"),
+        ("mines/goose","master","pressly/goose"),
+        ("mines/fosite","master","ory/fosite"),
+        #("gcapes/git-course","gh-pages","gcapes/git-course"),
+        ("mines/blaze","master","meteor/blaze"),
+        ("mines/h2","master","hyperium/h2"),
+        ("mines/coding-standard","master","slevomat/coding-standard"),
+        ("mines/picom","next","yshui/picom"),
+        ("mines/nodejs-logging","main","googleapis/nodejs-logging"),
+        ("mines/laravel-shopify","master","osiset/laravel-shopify"),
+        ("mines/puppetlabs-kubernetes","main","puppetlabs/puppetlabs-kubernetes"),
+        ("mines/volley","master","google/volley"),
+        ("mines/django-bootstrap4","main","zostera/django-bootstrap4"),
+        ("mines/databricks-cli","main","databricks/databricks-cli")
     ]
 
     i = 0
