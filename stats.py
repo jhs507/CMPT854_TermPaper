541cff3acb0d3e135f9398eb8a09e6cf4d43dbe1
jhs507 <jhs507@mail.usask.ca>
2022-12-01 11:45:21 -0600

    Added normality tests for all values.

--- stats.py
+++ stats.py
@@ -40,6 +40,61 @@ def main():
         alpha
     )
 
+    normality_test(
+        df["Inital Comments"],
+        "Inital Comment Lines",
+        alpha
+    )
+
+    normality_test(
+        df["Initial Average CCN"],
+        "Average CCN",
+        alpha
+    )
+
+    normality_test(
+        df["Functions"],
+        "Number of functions",
+        alpha
+    )
+
+    normality_test(
+        df["Complex Functions"],
+        "Complex functions",
+        alpha
+    )
+
+    normality_test(
+        df["Comment Ratio"],
+        "Comment Ratio",
+        alpha
+    )
+
+    normality_test(
+        df["Modularity Level"],
+        "Modularity level",
+        alpha
+    )
+
+    normality_test(
+        df["Documentation Level"],
+        "Documentation level",
+        alpha
+    )
+
+    normality_test(
+        df["Install"],
+        "Install",
+        alpha
+    )
+
+    normality_test(
+        df["Tutorial"],
+        "Tutorial",
+        alpha
+    )
+
+    
 
 if __name__ == "__main__":
     main()
